1c95fb0529eb2804fc90c5e29a45a70b7102ccb8
alvian-dave <alviandave69@gmail.com>
2025-06-16 16:18:42 +0700

    ngok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 // app/layout.tsx
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "World Reward Coin",
