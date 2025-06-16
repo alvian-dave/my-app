aec1dd84ababe6f1a94adfbc00b4511f6a9bfe36
alvian-dave <alviandave69@gmail.com>
2025-06-16 16:11:21 +0700

    aw

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+const tailwindcss = require('@tailwindcss/postcss')()
+
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    [tailwindcss.name]: tailwindcss, // Ini wajib pakai sintaks ini di Tailwind v4
     autoprefixer: {},
   },
-};
+}
